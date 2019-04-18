819eb78ca341ec7caeccbaae9a84a0ff4dad962b
Travis Tarpley <travistarpley0@gmail.com>
2019-04-17 17:45:33 -0700

    Added Nav Animation

--- load.js
+++ load.js
@@ -6,7 +6,17 @@ class LoadUp {
         this.window = $(window); 
         this.aboutTitle = $("#about-title"); 
         this.aboutCounter = 0; 
+        this.aboutContainer = $('#about'); 
+        this.workContainer = $('#work'); 
+        this.contactContainer = $('#contact'); 
         this.events(); 
+       
+
+        this.homeNav = $('#home-nav'); 
+        this.aboutNav = $('#about-nav'); 
+        this.workNav = $('#work-nav'); 
+        this.contactNav = $('#contact-nav'); 
+        this.lastPosition = this.homeNav; 
     }
 
     events() {
@@ -15,15 +25,71 @@ class LoadUp {
 		}); 
     }
     scrollEvent() {
+        // About Animation Scroll
         if (!this.aboutCounter == 1) {
-            if(this.window.scrollTop() > 300) {
+            if(this.window.scrollTop() >= this.aboutContainer.position().top) {
                 this.aboutTitle.addClass('about-title__animation'); 
                 this.aboutTitle.removeClass('unscrolled'); 
                 this.aboutCounter++; 
             }
-            
-        } else {
-            return; 
+        } 
+
+        //Home Nav Scroll  
+        if (this.window.scrollTop() < this.aboutContainer.position().top 
+           && this.lastPosition != this.homeNav) {
+            this.homeNav.removeClass('inactive'); 
+            this.homeNav.addClass('active'); 
+           
+            this.lastPosition.removeClass('active'); 
+            this.lastPosition.addClass('inactive'); 
+
+            this.lastPosition = this.homeNav; 
+        }
+
+      //About Nav Scroll  
+        if (this.window.scrollTop() >= this.aboutContainer.position().top 
+           && this.lastPosition != this.aboutNav) {
+            this.aboutNav.removeClass('inactive'); 
+            this.aboutNav.addClass('active'); 
+           
+            this.lastPosition.removeClass('active'); 
+            this.lastPosition.addClass('inactive'); 
+
+            this.lastPosition = this.aboutNav; 
+        }
+
+        //About Nav Scroll  
+        if (this.window.scrollTop() >= this.aboutContainer.position().top 
+           && this.lastPosition != this.aboutNav) {
+            this.aboutNav.removeClass('inactive'); 
+            this.aboutNav.addClass('active'); 
+           
+            this.lastPosition.removeClass('active'); 
+            this.lastPosition.addClass('inactive'); 
+
+            this.lastPosition = this.aboutNav; 
+        }
+        //Work Nav Scroll 
+        if (this.window.scrollTop() >= this.workContainer.position().top 
+           && this.lastPosition != this.workNav) {
+            this.workNav.removeClass('inactive'); 
+            this.workNav.addClass('active'); 
+           
+            this.lastPosition.removeClass('active'); 
+            this.lastPosition.addClass('inactive'); 
+
+            this.lastPosition = this.workNav; 
+        }
+
+        if (this.window.scrollTop() >= this.contactContainer.position().top - 50 
+           && this.lastPosition != this.contactNav) {
+            this.contactNav.removeClass('inactive'); 
+            this.contactNav.addClass('active'); 
+           
+            this.lastPosition.removeClass('active'); 
+            this.lastPosition.addClass('inactive'); 
+
+            this.lastPosition = this.contactNav; 
         }
     }
 }
