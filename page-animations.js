6753fac0f7d8adbb029590f3bb140b39a26f9031
BuildTools <unconfigured@null.spigotmc.org>
2019-05-04 14:35:51 -0700

    added animations

--- page-animations.js
+++ page-animations.js
@@ -4,9 +4,12 @@ class pageAnimation {
     constructor() {
         this.window = $(window); 
         this.aboutCounter = 0; 
-        this.projectCounter = 0; 
+        this.workCounter = 0; 
         this.aboutTitle = $("#about-title"); 
         this.aboutBody = $('#about-body'); 
+        this.workBody = $('#work-body'); 
+        this.workTitle = $('#work-title'); 
+
         this.aboutContainer = $('#about'); 
         this.workContainer = $('#work'); 
         this.contactContainer = $('#contact'); 
@@ -33,6 +36,19 @@ class pageAnimation {
                
             }
         } 
+
+        // Project Section 
+        if(!this.workCounter == 1) {
+            if(this.window.scrollTop() >= this.workContainer.position().top - 200) {
+                   //Title
+                   this.workTitle.addClass('about-title__animation'); 
+                   this.workTitle.removeClass('unscrolled_about_title'); 
+                   //Body Section
+                   this.workBody.addClass('about-body__animation'); 
+                   this.workBody.removeClass('unscrolled_about_body'); 
+                   this.workCounter++; 
+            }
+        }
     }
 
 
